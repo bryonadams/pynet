9795f4b0fa23bf7b268d5426f3f61cdec4f6a9ad
Bryon Adams <bryonadams89@gmail.com>
2016-12-10 18:10:26 -0500

    notes-functions1.txt lost on old rpi

---
+++ week6-1_multiply-fn.py
@@ -0,0 +1,16 @@
+#########################################################
+#  December 10th, 2016                                  #
+#   Create a function that returns the multiplication   #
+#   product of three parameters--x, y, and z. z should  #
+#   have a default value of 1.                          #
+#    a. Call the function with all positional           #
+#        arguments.                                     #
+#    b. Call the function with all named arguments.     #
+#    c. Call the function with a mix of positional and  #
+#       named arguments.                                #
+#    d. Call the function with only two arguments and   #
+#       use the default value for z.                    #
+#                                                       #
+#########################################################
+
+
