3c7b10f646c480179a67cdc36aa5962904bef888
Bryon Adams <bryonadams89@gmail.com>
2016-12-12 10:27:03 -0500

    pynet course function library

---
+++ pynet_lib.py
@@ -0,0 +1,11 @@
+########################################################
+# Library to store functions for PyNet course.         #
+#  I'd like to keep the naming convention for my file  #
+#  so I made this to dump everything in. This should   #
+#  also serve to keep importing simple.                #
+#                                                      #
+########################################################
+
+# Week 6-1, create a multiplication function. Default value of z is 1
+def xyz_mult(x, y, z=1):
+  return x * y * z
