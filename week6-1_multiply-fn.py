899e813ba4b8035990d14dc233f411e2e516b6df
Bryon Adams <bryonadams89@gmail.com>
2016-12-12 10:26:42 -0500

    week6-1 exercise

--- week6-1_multiply-fn.py
+++ week6-1_multiply-fn.py
@@ -1,7 +1,7 @@
 #########################################################
 #  December 10th, 2016                                  #
 #   Create a function that returns the multiplication   #
-#   product of three parameters--x, y, and z. z should  #
+#   product of three parameters: x, y, and z. z should  #
 #   have a default value of 1.                          #
 #    a. Call the function with all positional           #
 #        arguments.                                     #
@@ -11,6 +11,28 @@
 #    d. Call the function with only two arguments and   #
 #       use the default value for z.                    #
 #                                                       #
+#    Note: Functions stored in pynet_lib.py             #
+#                                                       #
 #########################################################
 
+# Function, stored in pynet_lib.py
+def xyz_mult(x, y, z=1):
+  return x * y * z
+
+# Output from interpreter
+>>> import pynet_lib
+>>> from pynet_lib import xyz_mult
+
+# Part a
+>>> xyz_mult(2, 5, 10)
+100
+# Part b
+>>> xyz_mult(x=5, y=5, z=10)
+250
+# Part c
+>>> xyz_mult(5, z=5, y=10)
+250
+# Part d
+>>> xyz_mult(5, 5)
+25
 
